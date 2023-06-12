72529582e289c21cf0165a36a918b5e0405dbddd
rodrigollo <rodrigogollo.nh@gmail.com>
2023-06-11 21:03:26 -0300

    fix port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { getAllGamesByListId, getClipsByBroadcaster } from './repositories/twitc
 import { getTwitchToken } from './config/twitch'
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(cors())
 
 app.get('/', (req, res) => {
